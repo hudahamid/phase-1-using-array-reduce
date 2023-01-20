177f4c589d301d409540a53f2e87627a60d2cce5
hudahamid <huda.00091581@gmail.com>
2023-01-20 20:59:34 +0300

    finish

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// Code your solution here
+const totalBatteries = batteryBatches.reduce((prev, current) => {
+  return prev + current;
+});
+console.log(totalBatteries);
